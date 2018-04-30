b1b33e67d8f9353d2db8dccc72c315987e718e0d
py <1024607182@qq.com>
2018-05-01 00:22:30 +0800

    1: commit demo project

---
+++ MCDemo/MCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCDemo
+//
+//  Created by py on 2018/5/1.
+//  Copyright © 2018年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

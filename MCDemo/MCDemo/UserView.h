4ca226edbaec7c7ee66533960ae74902c936e3b4
py <1024607182@qq.com>
2018-05-01 00:58:41 +0800

    1：delegate中增加代码 2：增加新的测试view

---
+++ MCDemo/MCDemo/UserView.h
@@ -0,0 +1,13 @@
+//
+//  UserView.h
+//  MCDemo
+//
+//  Created by py on 2018/5/1.
+//  Copyright © 2018年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserView : UIView
+
+@end
---
+++ MCDemo/MCDemo/UserView.m
@@ -0,0 +1,21 @@
+//
+//  UserView.m
+//  MCDemo
+//
+//  Created by py on 2018/5/1.
+//  Copyright © 2018年 future. All rights reserved.
+//
+
+#import "UserView.h"
+
+@implementation UserView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

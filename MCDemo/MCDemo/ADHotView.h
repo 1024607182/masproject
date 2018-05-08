b6bcb340b2fd1ed6d27f377aa2b26f06f59c180f
py <1024607182@qq.com>
2018-05-08 23:56:23 +0800

    1： add ADHotView 文件

---
+++ MCDemo/MCDemo/ADHotView.h
@@ -0,0 +1,13 @@
+//
+//  ADHotView.h
+//  MCDemo
+//
+//  Created by py on 2018/5/8.
+//  Copyright © 2018年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADHotView : UIView
+
+@end
---
+++ MCDemo/MCDemo/ADHotView.m
@@ -0,0 +1,21 @@
+//
+//  ADHotView.m
+//  MCDemo
+//
+//  Created by py on 2018/5/8.
+//  Copyright © 2018年 future. All rights reserved.
+//
+
+#import "ADHotView.h"
+
+@implementation ADHotView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

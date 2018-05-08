2bd7063cb6a05372de1d9288083a3e08fdea0711
py <1024607182@qq.com>
2018-05-09 00:39:17 +0800

    2： add dirle

---
+++ MCDemo/MCDemo/CarView.m
@@ -0,0 +1,21 @@
+//
+//  CarView.m
+//  MCDemo
+//
+//  Created by py on 2018/5/9.
+//  Copyright © 2018年 future. All rights reserved.
+//
+
+#import "CarView.h"
+
+@implementation CarView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- MCDemo/MCDemo/ViewController.m
+++ MCDemo/MCDemo/ViewController.m
@@ -21,6 +21,12 @@
     UIAlertController *alert = [[UIAlertController alloc] init];
     UIButton *btton = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.view addSubview:btton];
+    
+    UILabel *label = [[UILabel alloc] init];
+    label.backgroundColor = [UIColor redColor];
+    [self.view addSubview:label];
+    
+    
     UIView *test = [[UIView alloc] init];
     [self.view addSubview:test];
     
